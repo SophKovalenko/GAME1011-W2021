e4a7060114c9aa39539d0de7ae226b8b1df3593a
Sophia Kovalenko <sophia.kovalenko@georgebrown.ca>
2021-01-18 14:04:43 -0500

    Created and organized getters and setters

--- WEEK 1/Platform.h
+++ WEEK 1/Platform.h
@@ -4,15 +4,108 @@ using namespace std;
 
 class Platform
 {
+private: 
+	string m_platformName,
+		   m_manufacturer;
+	//array of games
 
+public:
+	Platform()
+	{
+		setPlatformName();
+		setManufacturer();
+
+	}
+	void setPlatformName()
+	{
+
+	}
+	string getPlatformName()
+	{
+
+	}
+	void setManufacturer()
+	{
+	
+	}
+	string getManufacturer()
+	{
+
+	}
 };
 
-class Game
+class Game : public Platform
 {
+private: 
+	string m_gameName,
+		   m_gamePublisher,
+	       m_gameDeveloper;
+	//array of achievments
 
+public:
+	Game()
+	{
+		setGameName();
+		setGamePublisher();
+		setGameDeveloper();
+
+	}
+	void setGameName()
+	{
+
+	}
+	string getGameName()
+	{
+
+	}
+	void setGamePublisher()
+	{
+
+	}
+	string getGamePublisher()
+	{
+
+	}
+	void setGameDeveloper()
+	{
+
+	}
+	string getGameDeveloper()
+	{
+	
+	}
 };
 
-class Achievement
+class Achievement : public Game
 {
+private:
+	string m_achieveTitle,
+		   m_achieveDescription;
+	int    m_achieveScore;
 
+public:
+	void setAchieveTitle()
+	{
+
+	}
+	string getAchieveTitle()
+	{
+
+	}	
+	void setAchieveDescription()
+	{
+
+	}
+	string getAchieveDescription()
+	{
+
+	}	
+	void setAchieveScore()
+	{
+
+	}
+	int getAchieveScore()
+	{
+
+	}
 };
