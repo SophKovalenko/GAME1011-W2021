f9881b9c8b1bedade154d4379703579ac15b521f
Sophia Kovalenko <sophia.kovalenko@georgebrown.ca>
2021-01-21 14:59:47 -0500

    edited platforms to meet requirements

--- WEEK 1/main.cpp
+++ WEEK 1/main.cpp
@@ -9,11 +9,28 @@ int main()
 	cout << "Welcome to the Hall of Acievements!" << endl;
 	cout << "Please select the number of the platform you would like to access (1-3)" << endl;
 	cout << "***********************************************************************" << endl << endl;
-	cout << "1. Orange Games " << endl;
-	cout << "2. Lemon Games " << endl;
-	cout << "3. Grapefruit Games " << endl;
+	cout << "1. PlayStation " << endl;
+	cout << "2. Xbox " << endl;
+	cout << "3. Nintendo Switch " << endl;
+	cout << "4. PC " << endl;
+	cout << "5. IoS/ Andriod " << endl;
 	cin >> choice;
 	cout << endl;
+	
+	switch (choice)
+	{
+	case 1: Platform ;
+
+	case 2: Platform ;
+
+	case 3: Platform ;
+
+	case 4: Platform;
+
+	case 5: Platform;
+
+	default: cout << "You have made an incorrect selection" << endl;
+	}
 
 	/* << "Please select the number of the game you would like to access (1-3)" << endl;
 	cout << "*******************************************************************" << endl << endl;
