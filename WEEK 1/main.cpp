20c336ec423a079702c8bcaa1dd7a238972008e9
Sophia Kovalenko <sophia.kovalenko@georgebrown.ca>
2021-01-22 19:51:29 -0500

    fixed final bugs

--- WEEK 1/main.cpp
+++ WEEK 1/main.cpp
@@ -7,21 +7,21 @@ int main()
 {
 	int choice;
 	const int numPlatforms = 5;
-	const int numGames = 5;
+	const int numGames = 6;
 	int gameNum, achieveNum;
 	string achieveTitle, achieveDesc, achieveScore;
 	Achievement* pAchieve = nullptr;
 	//Game* pGame = nullptr;
 
 
-	cout << "Welcome to the Hall of Acievements!" << endl;
+	cout << "Welcome to the Hall of Achievements!" << endl;
 	cout << "Please select the number of the platform you would like to access (1-5)" << endl;
 	cout << "***********************************************************************" << endl << endl;
 	cout << "1. PlayStation " << endl;
 	cout << "2. Xbox " << endl;
 	cout << "3. Switch " << endl;
 	cout << "4. PC " << endl;
-	cout << "5. Android " << endl << endl;
+	cout << "5. Android " << endl;
 	cin >> choice;
 	cout << endl;
 
@@ -39,6 +39,7 @@ int main()
 
 		Game game[numGames] = //array of Game objects
 		{
+		  Game("", "", ""),
 		  Game("Persona 5", "Atlus", "P-Studio"),
 		  Game("Until Dawn", "Sony Computer Entertainment", "Supermassive Games"),
 		  Game("Fall Guys", "Devolver Digital", "Mediatonic"),
@@ -53,34 +54,53 @@ int main()
 			cout << game[index].getGameName() << endl;
 		}
 
+		
+		cout << endl;
 		cout << "Which game would you like to create an achievement for?" << endl;
 		cin >> gameNum;
 
+		cout << endl;
+
+		cout << "You have chosen " << game[gameNum].getGameName() << endl;
+
 		cout << "How many achievements would you like to create?" << endl;
 		cin >> achieveNum;
+
+		cout << endl;
+		cin.ignore();
 
 		for (int count = 0; count < achieveNum; count++)
 		{
 			cout << "Please enter the achievement title" << endl;
 			getline(cin, achieveTitle);
+			cout << endl;
 
 			cout << "Please enter the achievement description" << endl;
 			getline(cin, achieveDesc);
+			cout << endl; 
 
 			cout << "Please enter the achievement score" << endl;
 			getline(cin, achieveScore);
+			cout << endl;
 
-			pAchieve = new Achievement("", "", "");
+			pAchieve = new Achievement();
 			pAchieve->setAchieve(achieveTitle, achieveDesc, achieveScore);
 
-			cout << "You have created the " << pAchieve->getAchieveTitle() << endl;
+			cout << endl;
+			cout << "*****************************************" << endl;
+			cout << endl;
+
+			cout << "You have created a new achievement! " << endl;
+			cout << pAchieve->getAchieveTitle() << endl;
 			cout << pAchieve->getAchieveDescription() << endl;
 			cout << pAchieve->getAchieveScore() << endl;
-
+			cout << endl;
+			
 			delete pAchieve;
 			pAchieve = nullptr;
 		}
 		
+
 	}
 
 	if (choice == 2)
@@ -89,6 +109,7 @@ int main()
 
 		Game game[numGames] = //array of Game objects
 		{
+		  Game("", "", ""),
 		  Game("Fortnight", "Epic Games", "Warner Bros. Interactive Entertainment"),
 		  Game("Grand Theft Auto V", "Rockstar Games", "Rockstar North"),
 		  Game("Apex Legends", "Electronic Arts", "Respawn Entertainment"),
@@ -103,6 +124,51 @@ int main()
 			cout << game[index].getGameName() << endl;
 		}
 
+
+		cout << endl;
+		cout << "Which game would you like to create an achievement for?" << endl;
+		cin >> gameNum;
+
+		cout << endl;
+
+		cout << "You have chosen " << game[gameNum].getGameName() << endl;
+
+		cout << "How many achievements would you like to create?" << endl;
+		cin >> achieveNum;
+
+		cout << endl;
+		cin.ignore();
+
+		for (int count = 0; count < achieveNum; count++)
+		{
+			cout << "Please enter the achievement title" << endl;
+			getline(cin, achieveTitle);
+			cout << endl;
+
+			cout << "Please enter the achievement description" << endl;
+			getline(cin, achieveDesc);
+			cout << endl;
+
+			cout << "Please enter the achievement score" << endl;
+			getline(cin, achieveScore);
+			cout << endl;
+
+			pAchieve = new Achievement();
+			pAchieve->setAchieve(achieveTitle, achieveDesc, achieveScore);
+
+			cout << endl;
+			cout << "*****************************************" << endl;
+			cout << endl;
+
+			cout << "You have created a new achievement! " << endl;
+			cout << pAchieve->getAchieveTitle() << endl;
+			cout << pAchieve->getAchieveDescription() << endl;
+			cout << pAchieve->getAchieveScore() << endl;
+			cout << endl;
+
+			delete pAchieve;
+			pAchieve = nullptr;
+		}
 	}
 
 	if (choice == 3)
@@ -111,6 +177,7 @@ int main()
 
 		Game game[numGames] = //array of Game objects
 		{
+		  Game("", "", ""),
 		  Game("SuperMario 3D AllStars", "Nintendo", "Nintendo EPD"),
 		  Game("Animal Crossing","Nintendo", "Nintendo EPD"),
 		  Game("Hades", "Supergiant Games", "Supergiant Games"),
@@ -125,7 +192,50 @@ int main()
 			cout << game[index].getGameName() << endl;
 		}
 
+		cout << endl;
+		cout << "Which game would you like to create an achievement for?" << endl;
+		cin >> gameNum;
 
+		cout << endl;
+
+		cout << "You have chosen " << game[gameNum].getGameName() << endl;
+
+		cout << "How many achievements would you like to create?" << endl;
+		cin >> achieveNum;
+
+		cout << endl;
+		cin.ignore();
+
+		for (int count = 0; count < achieveNum; count++)
+		{
+			cout << "Please enter the achievement title" << endl;
+			getline(cin, achieveTitle);
+			cout << endl;
+
+			cout << "Please enter the achievement description" << endl;
+			getline(cin, achieveDesc);
+			cout << endl;
+
+			cout << "Please enter the achievement score" << endl;
+			getline(cin, achieveScore);
+			cout << endl;
+
+			pAchieve = new Achievement();
+			pAchieve->setAchieve(achieveTitle, achieveDesc, achieveScore);
+
+			cout << endl;
+			cout << "*****************************************" << endl;
+			cout << endl;
+
+			cout << "You have created a new achievement! " << endl;
+			cout << pAchieve->getAchieveTitle() << endl;
+			cout << pAchieve->getAchieveDescription() << endl;
+			cout << pAchieve->getAchieveScore() << endl;
+			cout << endl;
+
+			delete pAchieve;
+			pAchieve = nullptr;
+		}
 	}
 
 	if (choice == 4)
@@ -134,6 +244,7 @@ int main()
 
 		Game game[numGames] = //array of Game objects
 		{
+		  Game("", "", ""),
 		  Game("Control", "505 Games", "Remedy Entertainment"),
 		  Game("Desperados 3", "THQ Nordic", "Mimimi Games"),
 		  Game("Disco Elysium", "ZA/UM", "ZA/UM"),
@@ -148,6 +259,51 @@ int main()
 			cout << game[index].getGameName() << endl;
 		}
 
+
+		cout << endl;
+		cout << "Which game would you like to create an achievement for?" << endl;
+		cin >> gameNum;
+
+		cout << endl;
+
+		cout << "You have chosen " << game[gameNum].getGameName() << endl;
+
+		cout << "How many achievements would you like to create?" << endl;
+		cin >> achieveNum;
+
+		cout << endl;
+		cin.ignore();
+
+		for (int count = 0; count < achieveNum; count++)
+		{
+			cout << "Please enter the achievement title" << endl;
+			getline(cin, achieveTitle);
+			cout << endl;
+
+			cout << "Please enter the achievement description" << endl;
+			getline(cin, achieveDesc);
+			cout << endl;
+
+			cout << "Please enter the achievement score" << endl;
+			getline(cin, achieveScore);
+			cout << endl;
+
+			pAchieve = new Achievement();
+			pAchieve->setAchieve(achieveTitle, achieveDesc, achieveScore);
+
+			cout << endl;
+			cout << "*****************************************" << endl;
+			cout << endl;
+
+			cout << "You have created a new achievement! " << endl;
+			cout << pAchieve->getAchieveTitle() << endl;
+			cout << pAchieve->getAchieveDescription() << endl;
+			cout << pAchieve->getAchieveScore() << endl;
+			cout << endl;
+
+			delete pAchieve;
+			pAchieve = nullptr;
+		}
 	}
 
 	if (choice == 5)
@@ -155,7 +311,8 @@ int main()
 		cout << "You have chosen the " << platform[4].getPlatformName() << " from " << platform[4].getManufacturer() << endl; // display platform info
 
 		Game game[numGames] = //array of Game objects
-		{
+		{ 
+		  Game("", "", ""),
 		  Game("Among Us", "Innersloth LLC", "Innersloth LLC"),
 		  Game("Robolox", "David Baszucki", "Robolox Corp."),
 		  Game("Clash of Clans", "Supercell", "Supercell"),
@@ -170,6 +327,50 @@ int main()
 			cout << game[index].getGameName() << endl;
 		}
 
+		cout << endl;
+		cout << "Which game would you like to create an achievement for?" << endl;
+		cin >> gameNum;
+
+		cout << endl;
+
+		cout << "You have chosen " << game[gameNum].getGameName() << endl;
+
+		cout << "How many achievements would you like to create?" << endl;
+		cin >> achieveNum;
+
+		cout << endl;
+		cin.ignore();
+
+		for (int count = 0; count < achieveNum; count++)
+		{
+			cout << "Please enter the achievement title" << endl;
+			getline(cin, achieveTitle);
+			cout << endl;
+
+			cout << "Please enter the achievement description" << endl;
+			getline(cin, achieveDesc);
+			cout << endl;
+
+			cout << "Please enter the achievement score" << endl;
+			getline(cin, achieveScore);
+			cout << endl;
+
+			pAchieve = new Achievement();
+			pAchieve->setAchieve(achieveTitle, achieveDesc, achieveScore);
+
+			cout << endl;
+			cout << "*****************************************" << endl;
+			cout << endl;
+
+			cout << "You have created a new achievement! " << endl;
+			cout << pAchieve->getAchieveTitle() << endl;
+			cout << pAchieve->getAchieveDescription() << endl;
+			cout << pAchieve->getAchieveScore() << endl;
+			cout << endl;
+
+			delete pAchieve;
+			pAchieve = nullptr;
+		}
 	}
 
 
